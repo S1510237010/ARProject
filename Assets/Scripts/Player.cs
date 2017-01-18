ff10403ffb36f1c243fb584cd97687384c0ba8c9
Lisa Gonaus <lisa.gonaus@students.fh-hagenberg.at>
2017-01-18 23:41:27 +0100

    Added Highscore Screen, Highscore Item Prefab and basic Player/Highscore classes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerData data;
+    public GameObject playerObject;
+    public HighscoreData Highscore;
+    
+
+    public void ItemCollected(int points)
+    {
+        data.Score += points;
+    }
+
+    public void onRun()
+    {
+        //TODO: Play jump SFX
+        //AudioSource.PlayClipAtPoint(SFK ,gameObject.transform.position);
+    }
+
+    public void onJump()
+    {
+        //TODO: Play jump SFX
+        //AudioSource.PlayClipAtPoint(SFK ,gameObject.transform.position);
+    }
+
+    public void onKill()
+    {
+        //TODO: Play death SFX
+        //AudioSource.PlayClipAtPoint(SFK ,gameObject.transform.position);
+        Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        if (Highscore == null)
+        {
+            Highscore = new HighscoreData();
+            Highscore.addItem(data);
+        }
+    }
+
+
+    // Use this for initialization
+	void Start () {
+		//TODO: Get PlayerName
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI/HighscoreData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreData : MonoBehaviour
+{
+    private static List<PlayerData> highscoreData;
+
+    // Use this for initialization
+    void Start()
+    {
+        highscoreData = new List<PlayerData>();
+        int listSize = UnityEngine.Random.Range(3, 20);
+        for (int i = 0; i < 10; i++)
+        {
+            addItem("Player " + (i + 1), UnityEngine.Random.Range(0, 9999));
+        }
+    }
+
+    public void addItem(PlayerData player)
+    {
+        highscoreData.Add(player);
+    }
+
+    public void addItem(String name, int score)
+    {
+        PlayerData p = new PlayerData();
+        p.PlayerName = name;
+        p.Score = score;
+        addItem(p);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

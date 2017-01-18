6763c0e7bd250c38c49773739c4bd9003f846513
Lisa Gonaus <lisa.gonaus@students.fh-hagenberg.at>
2017-01-18 22:05:11 +0100

    Added Title Screen and Basic Navigation

---
+++ Assets/Scripts/Navigation/NavigateToScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateToScene : MonoBehaviour {
+
+	//Navigate to Scene by ID
+    public void GoToScene(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    //Navigate to Scene by Name
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

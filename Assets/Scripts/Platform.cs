dd81f7a76acb5f07757d5fd7fb3d2dc7d4748428
Lisa Gonaus <lisa.gonaus@students.fh-hagenberg.at>
2017-01-30 21:36:07 +0100

    Added Platform class to toggel the platform collission detection on and of at an given interval.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	public bool fading;
+	[Range(0,120)]
+	public int fadeOutInterval;
+	[Range(0,120)]
+	public int fadeInInterval;
+	public Material activeMaterial;
+	public Material disabledMaterial;
+
+	// Use this for initialization
+	void Start () {
+		if (fading) {
+			StartCoroutine (fade(true));
+		}
+	}
+
+	IEnumerator fade (bool fadeOut){
+		if (fadeOut) {
+			yield return new WaitForSeconds(fadeOutInterval);
+		} else {
+			yield return new WaitForSeconds(fadeInInterval);
+		}
+		if (fading) {
+			StartCoroutine (fade (!fadeOut));
+		}
+		if(gameObject != null){
+			BoxCollider collider = gameObject.GetComponent<BoxCollider> ();
+			if (collider != null) {
+				collider.enabled = !collider.enabled;
+				Debug.Log ("Active: " + collider.enabled);
+				setMaterial (collider.enabled);
+			}
+		}
+	}
+
+	public void setMaterial(bool active){
+		Renderer renderer = GetComponent<Renderer> ();
+		if (active) {
+			renderer.material = activeMaterial;
+		}else{
+			renderer.material = disabledMaterial;
+		}
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

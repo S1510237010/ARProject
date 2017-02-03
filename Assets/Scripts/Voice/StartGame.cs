89b8f509a14c7da1885097e8a348d2f42c1cd386
EvaJobst <S1510237014@students.fh-hagenberg.at>
2017-02-03 11:25:37 +0100

    Added keywordRecognizer

--- Assets/Scripts/Voice/StartGame.cs
+++ Assets/Scripts/Voice/StartGame.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class StartGame : MonoBehaviour {
 
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
 	// Use this for initialization
 	void Start () {
-		
+        keywords.Add("Start Game", () =>
+        {
+            this.BroadcastMessage("OnStart");
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
 	}
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction)) {
+            keywordAction.Invoke();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {

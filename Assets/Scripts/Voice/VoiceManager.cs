f3e87737d3714e92aef9913470a741b00ffe9028
EvaJobst <S1510237014@students.fh-hagenberg.at>
2017-02-06 16:11:43 +0100

    Moved voice commands to manager class and added "Restart Game"

--- Assets/Scripts/Voice/VoiceManager.cs
+++ Assets/Scripts/Voice/VoiceManager.cs
@@ -1,28 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public class StartGame : MonoBehaviour
-{
+public class VoiceManager : MonoBehaviour {
+    public string[] possibleKeywords;
     KeywordRecognizer keywordRecognizer = null;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
     // Use this for initialization
-    void Start()
-    {
-        keywords.Add("Start Game", () =>
-        {
-            // Call the OnReset method on every descendant object.
-            this.BroadcastMessage("onStart");
-        });
+    void Start () {
 
+        for (int i = 0; i < possibleKeywords.Length; i++) {
+            switch (possibleKeywords[i]) {
+                case "Start Game":
+                    keywords.Add("Start Game", () => { NavigateToScene.GoToScene("Levels");
+                    });
+                    break; 
+
+                case "Exit Game":
+                    keywords.Add("Exit Game", () => { Application.Quit();
+                    });
+                    break;
+
+                case "Restart Game":
+                    keywords.Add("Restart Game", () => { NavigateToScene.GoToScene("Levels");
+                    });
+                    break;
+            }
+        }
         // Tell the KeywordRecognizer about our keywords.
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
 
         // Register a callback for the KeywordRecognizer and start recognizing!
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
+
     }
 
     private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
@@ -32,12 +46,5 @@ public class StartGame : MonoBehaviour
         {
             keywordAction.Invoke();
         }
-    }
-
-    void onStart()
-    {
-        //TODO: Segue to Tutorial Level
-        print("Said Start Game");
-		NavigateToScene.GoToScene("Levels");
     }
 }
